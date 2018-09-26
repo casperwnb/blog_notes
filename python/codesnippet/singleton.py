e7f57e6e272352006b7641b6fad36702c9c169f2
casperwnb <842590178@qq.com>
2018-09-26 18:12:57 +0800

    m

---
+++ python/codesnippet/singleton.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+
+def make_synchronized(func):
+    import threading
+
+    func.__lock__ = threading.Lock()
+    def synced_func(*args, **kwargs):
+        with func.__lock__:
+            return func(*args, **kwargs)
+    return synced_func
+
+class Singleton(object):
+    _instance = None
+
+    @make_synchronized
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
+
+class Pool(Singleton):
+    __first_init = False  #
+    def __init__(self):
+        if self.__class__.__first_init:
+            return
+        # self.pool = redis.ConnectionPool(host=host, port=port)
+        self.pool = {"a": 10}
+        self.__class__.__first_init = True
+
+    def redis_instance(self):
+        # redisPoolClient = redis.Redis(connection_pool=self.pool)
+        # return redisPoolClient
+        redisPool = self.pool
+        print(id(redisPool))
+
+
+def test1():
+    r1 = Pool().redis_instance()
+    r2 = Pool().redis_instance()
+
+test1()
